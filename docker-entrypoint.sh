0e56d51f44fe21b8a4e11274e74fbe0879e5419f
3roiler <bleikindonline@gmail.com>
2025-11-16 19:01:54 +0000

    add envPath option to database migration command in entrypoint script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "[entrypoint] Running database migrations..."
-./node_modules/.bin/node-pg-migrate up -m ./migrations --single-transaction
+./node_modules/.bin/node-pg-migrate up -m ./migrations --single-transaction --envPath .env
 
 echo "[entrypoint] Starting API server..."
 exec node dist/app.js
