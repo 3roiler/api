3b129d2d777f74d7872a16af72a5bcaf6385d4dc
3roiler <bleikindonline@gmail.com>
2025-11-16 14:55:55 +0000

    test

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 set -e
 
-echo "[entrypoint] Running database migrations (SSL-aware wrapper)..."
-node -e "import('./dist/scripts/migrate.js').then(module => module.run())"
-
 echo "[entrypoint] Starting API server..."
 exec node dist/app.js
